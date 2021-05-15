654cd8168a6ab57625defb8af9527f7e007c7117
Ryely <ryely.moore@outlook.com>
2021-05-15 11:53:45 -0600

    generatePassword function filled in, call again conditions in passwordCriteria not functioning properly commented out for now.

--- script.js
+++ script.js
@@ -27,21 +27,21 @@ var passwordCriteria = function () {
 // Determine the length of the password, and sets character range
   var length = prompt("How long should the password be? Enter a length between 8 and 128 characters.");
   length = parseInt(length);
-  if (length <= 7 || length >= 129) {
-    alert("Please enter a number between 8 and 128.");
-    passwordCriteria();
-  }
+  // if (length <= 7 || length >= 129) {
+  //   alert("Please enter a number between 8 and 128.");
+  //   passwordCriteria();
+  // }
 // Determine what types of characters will be used
   var lower = confirm("Should the password contain lower case letters?");
   var upper = confirm("Should the password contain UPPER case letters?");
   var number = confirm("Should the password contain numbers?");
   var special = confirm("Should the password contain special characters? (@,!,#,etc.)");
 // Return the criteria as an array
-  var arr = [{'lower':lower},{'upper':upper},{'number':number},{'special':special},{'length':length}];
-  if (!arr.lower && !arr.upper && !arr.number && !arr.special) {
-    alert("At least one character type must be selected.");
-    passwordCriteria();
-  }
+  // var arr = [{'lower':lower},{'upper':upper},{'number':number},{'special':special},{'length':length}];
+  // if (!arr[0].lower && !arr[1].upper && !arr[2].number && !arr[3].special) {
+  //   alert("At least one character type must be selected.");
+  //   passwordCriteria();
+  // }
   return arr;
 }
 // returns a random character
@@ -50,27 +50,29 @@ var randomChar = function () {
   var lower = 'abcdefghijklmnopqrstuvwxyz';
   var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var number = '1234567890';
-  var symbol = '!#$%&()*+,-./:;<=>?@[\]^_`{|}~'
+  var special = '!#$%&()*+,-./:;<=>?@[\]^_`{|}~'
   // creates a random number between 1 and 4
   var choice = Math.floor(Math.random()*4);
+  // For each character type, take a random character and creates an object with the randomly chosen character and its type
   switch(choice){
     case 0:
       var x = lower[Math.floor(Math.random()*(lower.length+1))];
-      var char = {'lower': x};
+      var char = [{test:'lower'},{'value': x}];
       break;
     case 1:
       var x = upper[Math.floor(Math.random()*(upper.length+1))];
-      var char = {'upper': x};
+      var char = [{test:'upper'},{'value': x}];
       break;
     case 2:
       var x = number[Math.floor(Math.random()*(number.length+1))];
-      var char = {'number': x};
+      var char = [{test:'number'},{'value': x}];
       break;
     case 3:
-      var x = symbol[Math.floor(Math.random()*(symbol.length+1))];
-      var char = {'symbol': x};
+      var x = special[Math.floor(Math.random()*(special.length+1))];
+      var char = [{test:'special'},{'value': x}];
       break;
   }
+// returns the char object
   return char;
 }
 
@@ -79,10 +81,22 @@ var generatePassword = function () {
   var criteria = passwordCriteria();
   var password = '';
   // Creates over the length
-  for (var i = 0; i < criteria.length; i++) {
-    // Get a random lower, upper, number, or symbol then check if it is allowed based on criteria
-    var char = randomChar();
+  for (var i = 0; i < criteria[4].length; i++) {
+    // Get a random lower, upper, number, or special then check if it is allowed based on criteria
+    while (true) {
+      var char = randomChar();
+      if ((char[0].test === 'lower' && criteria[0].lower) ||
+          (char[0].test === 'upper' && criteria[1].upper) ||
+          (char[0].test === 'number' && criteria[2].number) ||
+          (char[0].test === 'special' && criteria[3].special)){
+            break;
+          }
+    }
+    // concats the generated character onto the password string
+    password = password.concat(char[1].value);
   }
+  // return the password string
+  return password;
 }
 
 // Write password to the #password input
